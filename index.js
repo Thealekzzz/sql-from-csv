f9cde604684c84704c37e20b21353df84fba32a1
Alex Kuznetsov <kznv.alex@yandex.ru>
2025-09-02 17:41:39 +0300

    feat: add join module

--- index.js
+++ index.js
@@ -1,143 +1,301 @@
-import { readFileSync, writeFileSync } from 'fs';
+import { readFileSync, writeFileSync, mkdirSync, readdirSync } from "fs";
+import { columns } from "./consts.js";
+import { extname, join } from "path";
 
-function createTableAndFillFromCSV(filename, tableName, columns, sep = ';', dumpSize = 65) {
+String.prototype.replaceAll = function (from, to) {
+    return this.split(from).join(to);
+};
 
-  // Read CSV data
-  const text = readFileSync(filename).toString();
+function createTableAndFillFromCSV(
+    tableName,
+    columns,
+    sep = ";",
+    dumpSize = 65
+) {
+    const chunkSize = 50000;
 
-  // Make columns description
-  const colNamesStringExtended = columns.map((column) => `\`${column.name}\` ${column.type}`).join(',\n');
-  const colNamesString = columns.map((column) => `\`${column.name}\``).join(',');
+    // Папки ввода и вывода
+    const inputDir = "./input";
+    const outputDir = "./result";
 
-  // Make rows data
-  const lines = text
-    .split('\n')
-    .map((line) => line
-      .split(sep)
-      .map((char) => `'${char.replace('\r', '')}'`)
-      .join(','))
-    .map((line) => `(${line})`);
+    // Создаем папку result, если она не существует
+    mkdirSync(outputDir, { recursive: true });
 
-  let dumps = '';
-  const numberOfLines = lines.length;
+    // Получаем список всех xlsx файлов в папке input
+    const files = readdirSync(inputDir);
+    const csvFiles = files.filter(
+        (file) => extname(file).toLowerCase() === ".csv"
+    );
 
-  // Make separated blocks of inserting data
-  for (let i = 0; i < numberOfLines; i += dumpSize) {
-    const dataBlock = lines.slice(i, Math.min(i + dumpSize, numberOfLines)).join(',\n');
-    const insertQuery = `INSERT INTO \`${tableName}\` (${colNamesString}) VALUES \n${dataBlock}`;
-    dumps += insertQuery + ';\n';
-  }
+    if (csvFiles.length === 0) {
+        console.log("В папке input не найдено xlsx файлов");
+        return;
+    }
 
-  return `
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+    let isCreatingTableCodeAdded = false;
 
-CREATE TABLE \`${tableName}\` (
-${colNamesStringExtended}
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+    for (const file of csvFiles) {
+        const filePath = join(inputDir, file);
+        const fileName = file.replace(".csv", "");
 
-${dumps}
-`;
+        // Read CSV data
+        const text = readFileSync(filePath).toString();
+
+        const data = text.split("\n");
+        const head = data[0].replace("\r", "").split(sep);
+
+        const rows = data.slice(1);
+
+        const duplicatesCounter = {};
+        const columnsToCheckDuplicates = [
+            "NAAB Code",
+            "InterRegNumber",
+            "Name",
+        ];
+        const columnNumbersToCheck = columnsToCheckDuplicates.map(
+            (columnName) =>
+                head.findIndex((headName) => headName === columnName)
+        );
+
+        const columnIndexesByDBColumnName = Object.fromEntries(
+            columns.map(
+                ({
+                    name: BDColumnName,
+                    columnNameInTable,
+                    ruColumnNameInTable,
+                }) => {
+                    let columnIndex = head.findIndex(
+                        (headName) => headName === columnNameInTable
+                    );
+
+                    if (columnIndex === -1) {
+                        columnIndex = head.findIndex(
+                            (headName) => headName === ruColumnNameInTable
+                        );
+                    }
+
+                    return [
+                        BDColumnName,
+                        columnIndex !== -1 ? columnIndex : undefined,
+                    ];
+                }
+            )
+        );
+
+        // Make columns description
+        const colNamesStringExtended = columns
+            .map((column) => `\`${column.name}\` ${column.type}`)
+            .join(",\n");
+
+        const columnsToInsertData = columns.filter(
+            ({ name: BDColumnName }) => BDColumnName !== "id"
+        );
+        const colNamesString = columnsToInsertData
+            .map((column) => `\`${column.name}\``)
+            .join(",");
+
+        // Make rows data
+        const lines = rows
+            .map((line) => {
+                const chars = line
+                    .split(sep)
+                    .map(
+                        (char) =>
+                            `'${char.replace("\r", "").replaceAll("\r", "")}'`
+                    );
+
+                const key = columnNumbersToCheck
+                    .map((number) => chars[number])
+                    .join("-");
+                if (duplicatesCounter[key]) {
+                    return null;
+                }
+
+                duplicatesCounter[key] = 1;
+
+                return columnsToInsertData
+                    .map(({ name: BDColumnName }) => {
+                        if (
+                            columnIndexesByDBColumnName[BDColumnName] !==
+                            undefined
+                        ) {
+                            const value =
+                                chars[
+                                    columnIndexesByDBColumnName[BDColumnName]
+                                ];
+
+                            if (
+                                BDColumnName === "birth_date" &&
+                                value &&
+                                value !== `''`
+                            ) {
+                                try {
+                                    const [m, d, y] = value
+                                        .replace(/'/g, "")
+                                        .split("/");
+                                    return (
+                                        "'" +
+                                        [
+                                            y,
+                                            m.padStart(2, "0"),
+                                            d.padStart(2, "0"),
+                                        ].join("-") +
+                                        "'"
+                                    );
+                                } catch (err) {
+                                    console.log(value);
+                                    console.log(err);
+                                }
+                            }
+
+                            if (value === "''") {
+                                return "NULL";
+                            }
+
+                            return value;
+                        }
+
+                        return "NULL";
+                    })
+                    .join(",");
+            })
+            .filter((line) => line !== null)
+            .map((line) => `(${line})`);
+
+        // Separate lines on chunks to create low-sized files
+        const numberOfLines = lines.length;
+        const chunks = Array(Math.ceil(numberOfLines / chunkSize))
+            .fill()
+            .map((_, index) =>
+                lines.slice(index * chunkSize, (index + 1) * chunkSize)
+            );
+
+        // console.log(lines);
+
+        for (let chunkIndex = 0; chunkIndex < chunks.length; chunkIndex++) {
+            const chunk = chunks[chunkIndex];
+            const numberOfLines = chunk.length;
+            let dumps = "";
+
+            // Make separated blocks of inserting data
+            for (let i = 0; i < numberOfLines; i += dumpSize) {
+                const dataBlock = chunk
+                    .slice(i, Math.min(i + dumpSize, numberOfLines))
+                    .join(",\n");
+                const insertQuery = `INSERT INTO \`${tableName}\` (${colNamesString}) VALUES \n${dataBlock}`;
+                dumps += insertQuery + ";\n";
+            }
+
+            let result = "";
+
+            if (shouldCreateTable && !isCreatingTableCodeAdded) {
+                result += `SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+  START TRANSACTION;
+  SET time_zone = "+00:00";
+  
+  CREATE TABLE \`${tableName}\` (
+  ${colNamesStringExtended}
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  
+  `;
+                isCreatingTableCodeAdded = true;
+            }
+
+            result += dumps;
+
+            const outputFilename = `${fileName}_${chunkIndex + 1}.sql`;
+            const outputPath = join(outputDir, outputFilename);
+            writeFileSync(outputPath, result);
+            console.log(
+                `Записан файл ${outputFilename} (source ${outputFilename})`
+            );
+        }
+    }
 }
 
-const columns = [
-  { name: 'NAAB Code', type: 'varchar(10) DEFAULT NULL' },
-  { name: 'InterRegNumber', type: 'varchar(18) DEFAULT NULL' },
-  { name: 'Name', type: 'varchar(20) DEFAULT NULL' },
-  { name: 'Full Name', type: 'varchar(50) DEFAULT NULL' },
-  { name: 'Breed', type: 'varchar(9) DEFAULT NULL' },
-  { name: 'TPI', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'NM$', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'CM$', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'FM$', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'GM$', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'Milk', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'Protein', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'Prot%', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'Fat', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'Fat %', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'CFP', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'FE', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'Feed Saved', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'Prel', type: 'varchar(3) DEFAULT NULL' },
-  { name: 'D / H', type: 'varchar(11) DEFAULT NULL' },
-  { name: 'PL', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'C-LIV', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'H-LIV', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'DPR', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'SCS', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'SCE', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'SCE Rel', type: 'varchar(3) DEFAULT NULL' },
-  { name: 'SCE Obs', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'DCE', type: 'varchar(3) DEFAULT NULL' },
-  { name: 'SSB', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'DSB', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'CCR', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'HCR', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'EFC', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'GL', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'MAST', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'KET', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'RP', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'MET', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'DA', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'MF', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'MS', type: 'varchar(3) DEFAULT NULL' },
-  { name: 'DWP$', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'WT$', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'CW$', type: 'int DEFAULT NULL' },
-  { name: 'PTAT', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'UDC', type: 'varchar(6) DEFAULT NULL' },
-  { name: 'FLC', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'BWC', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'DC', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'TRel', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'D / H2', type: 'varchar(11) DEFAULT NULL' },
-  { name: 'Stature', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'Strength', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'Body Depth', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'Dairy form', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'Rump Angle', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'Thurl Width', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'RLSV', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'RLRV', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'Foot Angle', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'FLS', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'F. Udder Att.', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'R Udder Height', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'Rear Udder Width', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'Udder Cleft', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'Udder Depth', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'FTP', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'RTP', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'RTP SV', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'Teat Length', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'Pedigree', type: 'varchar(90) DEFAULT NULL' },
-  { name: 'aAa', type: 'varchar(14) DEFAULT NULL' },
-  { name: 'DMS', type: 'varchar(16) DEFAULT NULL' },
-  { name: 'Kappa-Casein', type: 'varchar(5) DEFAULT NULL' },
-  { name: 'Beta-Casein', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'BBR', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'B-LACT', type: 'varchar(3) DEFAULT NULL' },
-  { name: 'Genetic Codes', type: 'varchar(34) DEFAULT NULL' },
-  { name: 'Haplotypes', type: 'varchar(37) DEFAULT NULL' },
-  { name: 'RHA', type: 'varchar(37) DEFAULT NULL' },
-  { name: 'EFI', type: 'varchar(4) DEFAULT NULL' },
-  { name: 'Birth Date', type: 'varchar(10) DEFAULT NULL' },
-  { name: 'Proof', type: 'varchar(10) DEFAULT NULL' },
-  { name: 'ADV', type: 'varchar(10) DEFAULT NULL' },
-  { name: 'GS', type: 'varchar(10) DEFAULT NULL' },
-  { name: 'FS', type: 'varchar(2) DEFAULT NULL' },
-  { name: '511', type: 'varchar(3) DEFAULT NULL' },
-  { name: 'EDGE', type: 'varchar(3) DEFAULT NULL' },
-  { name: 'CP', type: 'varchar(2) DEFAULT NULL' },
-  { name: 'CP511', type: 'varchar(5) DEFAULT NULL' }
-];
+function checkDuplicats(filename, sep = ";") {
+    const columnsToCheck = ["NAAB Код", "InterRegNumber"];
+    const counter = {};
 
-const filename = 'data-in-csv.txt';
-const resultFilename = 'query.sql';
-const tableName = 'testTable';
+    // Read CSV data
+    const text = readFileSync("./input/" + filename).toString();
 
-const resultQuery = createTableAndFillFromCSV(filename, tableName, columns);
-writeFileSync(resultFilename, resultQuery);
+    const data = text.split("\n");
+    const head = data[0].split(sep);
+
+    const rows = data.slice(1);
+
+    const columnNumbersToCheck = columnsToCheck.map((columnName) =>
+        head.findIndex((headName) => headName === columnName)
+    );
+
+    rows.forEach((row) => {
+        const values = row.split(sep);
+
+        const key = columnNumbersToCheck
+            .map((number) => values[number])
+            .join("-");
+
+        counter[key] = (counter[key] || 0) + 1;
+    });
+
+    console.log(
+        Object.entries(counter)
+            .filter(([key, value]) => value > 1)
+            .sort((a, b) => b[1] - a[1])
+    );
+
+    return counter;
+}
+
+const tableName = "AltaGenAugust2025";
+const breed = "HO";
+const shouldCreateTable = true;
+
+// const countDuplicates = checkDuplicats(filename);
+// console.log(countDuplicates);
+
+createTableAndFillFromCSV(tableName, columns);
+
+// const a = `NAAB Code;InterRegNumber;Name;Full Name;Breed;TPI;NM$;CM$;FM$;GM$;Milk;Protein;Prot%;Fat;Fat %;CFP;FE;Feed Saved;Prel;D / H;PL;C-LIV;H-LIV;FI;DPR;SCS;SCE;SCE Rel;SCE Obs;DCE;SSB;DSB;CCR;HCR;EFC;GL;MAST;KET;RP;MET;DA;MF;MS;DWP$;WT$;CW$;PTAT;UDC;FLC;BWC;DC;TRel;D / H;Stature;Strength;Body Depth;Dairy form;Rump Angle;Thurl Width;RLSV;RLRV;Foot Angle;FLS;F. Udder Att.;R Udder Height;Rear Udder Width;Udder Cleft;Udder Depth;FTP;RTP;Teat Length;Pedigree;aAa;DMS;Kappa-Casein;Beta-Casein;B-LACT;Genetic Codes;Haplotypes;RHA;EFI;Birth Date;Proof;ADV;GS;FS;511;EDGE;CP;CP511
+// string;string;string;string;string;integer;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;string;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;string;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;decimal;string;string;string;string;string;string;string;string;decimal;decimal;string;string;string;string;string;string;string;string;string`;
+
+// const b = a.split("\n");
+
+// const c = b[1].split(";");
+
+// const d = b[0].split(";").reduce(
+//   (acc, fieldName, index) => ({
+//     ...acc,
+//     [fieldName]: {
+//       type: c[index],
+//     },
+//   }),
+//   {}
+// );
+
+// console.log(JSON.stringify(d));
+
+// // Обновление колонок
+// function generateAlterTableSQL(tableName, columns) {
+//     // Начало SQL-запроса
+//     let sql = `ALTER TABLE ${tableName} `;
+
+//     // Массив для хранения изменений
+//     const changes = [];
+
+//     // Проходим по каждому столбцу
+//     columns.forEach(column => {
+//         // Формируем часть запроса для изменения столбца
+//         changes.push(`MODIFY COLUMN \`${column.name}\` ${column.type}`);
+//     });
+
+//     // Объединяем все изменения в один запрос
+//     sql += changes.join(", ") + ";";
+
+//     return sql;
+// }
+
+// // Пример использования
+// const sqlQuery = generateAlterTableSQL(tableName, columns);
+// console.log(sqlQuery);
